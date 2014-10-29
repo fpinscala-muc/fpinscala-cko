6d2bf18c4d6d13818f2b39fd88accf494742f90a
Christine Koppelt <cko@users.noreply.github.com>
2014-10-29 16:54:12 +0100

    one more exercise for chapter 6

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -72,7 +72,13 @@ object RNG {
     map(nonNegativeInt)(_/(Int.MaxValue.toDouble + 1))
   }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    rng => {
+      val (a, rng1) = ra(rng)
+      val (b, rng2) = rb(rng1)
+      (f(a,b), rng2)
+    }
+  }
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
